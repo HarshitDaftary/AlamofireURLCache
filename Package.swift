fbb648aab0830f80a433bb437b2d29469d7a05f3
Harshit Daftary <daftaryharshit@yahoo.co.in>
2021-01-19 23:05:24 +0530

    added file name

--- Package.swift
+++ Package.swift
@@ -21,7 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AlamofireURLCache5",
-            dependencies: [], path: "./AlamofireURLCache"),
+            dependencies: [],
+            path: "./AlamofireURLCache",
+            sources: ["AlamofireURLCache.swift"]),
         .testTarget(
             name: "AlamofireURLCache5Tests",
             dependencies: ["AlamofireURLCache5"]),
